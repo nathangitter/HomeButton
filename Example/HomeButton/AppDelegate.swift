39c5dbb0c7956a3875526431c8002228ed111cc5
Ian McDowell <me@ianmcdowell.net>
2018-03-16 15:13:27 -0700

    Automatic home button addition

--- Example/HomeButton/AppDelegate.swift
+++ Example/HomeButton/AppDelegate.swift
@@ -7,18 +7,14 @@
 //
 
 import UIKit
-import HomeButton
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-
-        HomeButton.add(to: application)
 
         return true
     }
