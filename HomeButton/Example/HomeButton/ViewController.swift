2bc2e5e89fc94af3141cfdf84c1ff63ca44d7000
Nathan Gitter <nathan@nathangitter.com>
2018-03-16 16:18:02 -0500

    pod lib create

---
+++ HomeButton/Example/HomeButton/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HomeButton
+//
+//  Created by nathangitter on 03/16/2018.
+//  Copyright (c) 2018 nathangitter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ HomeButton/Example/Pods/Target Support Files/HomeButton/HomeButton-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HomeButton : NSObject
+@end
+@implementation PodsDummy_HomeButton
+@end
